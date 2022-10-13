e79528d2a323896e331a06b5cd084611ee545c53
aritrajtman <47066506@est.ort.edu.ar>
2022-10-13 10:37:56 -0300

    Avances

---
+++ Assets/Scripts/CameraScriptLevel2.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScriptLevel2 : MonoBehaviour
+{
+    public CameraManager camManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ActivateCamera()
+    {
+        camManager.CameraActivation();
+    }
+}
