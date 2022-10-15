201d150d4c8b9a2a1e3f90ca9469b05de873a98f
ThiagoMinian <101407767+ThiagoMinian@users.noreply.github.com>
2022-10-15 05:05:57 +0200

    Avances

---
+++ Assets/Scripts/VolverAlJuego.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class VolverAlJuego : MonoBehaviour
+{
+    public void OnClick1()
+    {
+      SceneManager.LoadScene(6);
+
+       
+    }
+
+}
