16a0d5c52547cbeb31d1a15e73819ff74eac4280
ThiagoMinian <101407767+ThiagoMinian@users.noreply.github.com>
2022-10-15 03:41:42 +0200

    Avances

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -8,19 +8,19 @@ public class Checkpoint : MonoBehaviour
     {
         if (col.gameObject.name == "Deathwall1")
         {
-            transform.position = new Vector3(289.79f, -5.44f, 89.93f);
-            //transform.eulerAngles = new Vector3(0, -90, 0);
+            transform.position = new Vector3(287.24f, 0.174f, 0.027f);
+            transform.eulerAngles = new Vector3(0, -90, 0);
         }
 
         if (col.gameObject.name == "Deathwall2")
         {
-            transform.position = new Vector3(50.3f, -12.3f, 94.9f);
-            //transform.eulerAngles = new Vector3(0, -90, 0);
+            transform.position = new Vector3(248.012f, -2.34f, -0.53f);
+            transform.eulerAngles = new Vector3(0, -90, 0);
         }
         if (col.gameObject.name == "Deathwall3")
         {
             transform.position = new Vector3(42.6f, -6.1f, 0f);
-            //transform.eulerAngles = new Vector3(0, -90, 0);
+            transform.eulerAngles = new Vector3(0, -90, 0);
         }
     }
 }
