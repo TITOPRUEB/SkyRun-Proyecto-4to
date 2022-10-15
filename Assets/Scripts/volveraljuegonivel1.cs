ae18d3b7e9b5f19d1653b24e4599d91c9421af75
ThiagoMinian <101407767+ThiagoMinian@users.noreply.github.com>
2022-10-15 23:53:43 +0200

    Avances

---
+++ Assets/Scripts/volveraljuegonivel1.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class volveraljuegonivel1 : MonoBehaviour
+{
+
+    public void OnClick1()
+    {
+        SceneManager.LoadScene(4);
+
+
+    }
+}
---
+++ Assets/Scripts/volveraljuegonivel3.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class volveraljuegonivel3 : MonoBehaviour
+{
+    public void OnClick1()
+    {
+        SceneManager.LoadScene(8);
+
+
+    }
+}
