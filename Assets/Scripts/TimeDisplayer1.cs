f73110fa4101a711b7aeb6a6c232726673c4524d
PULICCE MATEO <46956808@ort>
2022-10-24 10:44:15 -0300

    trg

---
+++ Assets/Scripts/TimeDisplayer1.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeDisplayer1 : MonoBehaviour
+{
+    Text display;
+    GameTimerText GMTXT1;
+
+    void Start()
+    {
+        display = GetComponent<Text>();
+        GMTXT1 = FindObjectOfType<GameTimerText>();
+        if (GMTXT1)
+            display.text = "Tu tiempo fue de: " + GMTXT1.gameTimer + " segundos";
+    }
+}
