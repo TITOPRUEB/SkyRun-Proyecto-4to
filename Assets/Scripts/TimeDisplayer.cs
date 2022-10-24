cd5088a3c6fd794e28ffdd9ef1e537ae93773a6a
ThiagoMinian <101407767+ThiagoMinian@users.noreply.github.com>
2022-10-24 03:35:13 +0200

    AnimacionJugador

---
+++ Assets/Scripts/TimeDisplayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeDisplayer : MonoBehaviour
+{
+    Text display;
+    GameTimerText GMTXT2;
+    
+    void Start()
+    {
+        display = GetComponent<Text>();
+        GMTXT2 = FindObjectOfType<GameTimerText>();
+        if(GMTXT2)
+            display.text = "Tu tiempo fue de: " + GMTXT2.gameTimer+ " segundos";
+    }
+}
